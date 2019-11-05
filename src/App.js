d331e4997e5afb6f78633b1c4bc47f991bc49101
unknown <micposso@gmail.com>
2019-11-04 22:02:29 -0500

    add layout and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Dashboard from './Dashboard';
-
+import { Container, Row, Col } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Container>
+      <Row>
+      <Col>
+        <Dashboard />
+      </Col>
+      </Row>
+    </Container>
   );
 }
 
