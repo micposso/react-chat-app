ebb1ed4d30a8600424c6996cafa2bc9b4beb5040
unknown <micposso@gmail.com>
2019-11-06 22:34:46 -0500

    add layout components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Container>
-      <Row>
+      <Row className="justify-content-md-center">
       <Col>
         <Dashboard />
       </Col>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,26 +1,67 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from "react";
+import { Container, Row, Col, Dropdown, Button } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
-  },
+    padding: theme.spacing(3, 2)
+  }
 }));
 
 export default function Dashboard() {
   const classes = useStyles();
 
+  const topics = ["technology", "work", "office"];
+
+  const [textValue, changeTextValue] = React.useState("");
+
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        This is a sheet of paper.
-      </Typography>
-      <Typography component="p">
-        Paper can be used to build surface or other elements for your
-        application.
-      </Typography>
-    </Paper>
+    <Container>
+      <Paper className={classes.root}>
+        <Container>
+          <Row>
+            <Col xs={4}>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {topics.map((item, key) => (
+                    <Dropdown.Item key={item}>{item}</Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col>
+              <Typography variant="h1" component="h2">
+                {textValue}
+              </Typography>
+            </Col>
+          </Row>
+        </Container>
+      </Paper>
+
+      <Paper className={classes.root}>
+        <Container>
+          <Row>
+            <Col>
+              <TextField
+                id="outlined-basic"
+                label="send a chat"
+                className={classes.textField}
+                value={textValue}
+                onChange={e => changeTextValue(e.target.value)}
+                variant="outlined"
+              />
+              <Button>SEND</Button>
+            </Col>
+          </Row>
+        </Container>
+      </Paper>
+    </Container>
   );
 }
